c61e8306d916965ff3c7fe2421e64f0df1e489d2
Sergey <konsaltpns@gmail.com>
2024-12-30 10:14:38 +0200

    add hw 6

--- js/task-2.js
+++ js/task-2.js
@@ -1,17 +1,28 @@
-console.log("----------------- TASK 2 -------------------")
-const allUsers = [
-{name: "Moore Hensley", friends: ["Sharron Pace"]},
-{name: "Sharlene Bush", friends: ["Briana Decker", "Sharron Pace"]},
-{name: "Ross Vazquez",friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"]},
-{name: "Elma Head",friends: ["Goldie Gentry", "Aisha Tran"]},
-{name: "Carey Barr",friends: ["Jordan Sampson", "Eddie Strong"]},
-{name: "Blackburn Dotson",friends: ["Jacklyn Lucas", "Linda Chapman"]},
-{name: "Sheree Anthony",friends: ["Goldie Gentry", "Briana Decker"]}
-];
+class Storage{
+    #items;
+    constructor(items) {
+        this.#items = items;
+    }
+    getItems() { return this.#items; }; //— повертає масив поточних товарів у приватній властивості items.
+    
+    addItem(newItem) { 
+        this.#items.push(newItem);
+    }; //— приймає новий товар newItem і додає його до масиву товарів у приватну властивість items об'єкта.
+    
+    removeItem(itemToRemove) {
+        this.#items=this.#items.filter(item => item !== itemToRemove);
+     };// — приймає рядок з назвою товару itemToRemove і видаляє його з масиву товарів у приватній властивості items об'єкта.
+}
+console.log("======================= TASK 2 ==============================")
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
 
-const getUsersWithFriend = (users, friendName) => users.filter(user => user.friends.includes(friendName));
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
 
-console.log(getUsersWithFriend(allUsers, "Briana Decker"));
-console.log(getUsersWithFriend(allUsers, "Goldie Gentry"));
-console.log(getUsersWithFriend(allUsers, "Adrian Cross" ));
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
+storage.removeItem("Scaner");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
 console.log("");
